c6e615b130910ba0fef5f0d9c9e312e201dc91c8
Duy <daovanduy0311@gmail.com>
2017-01-09 15:03:36 +0700

    ff

--- SDKSystem/app/src/main/java/com/hitapps/sdksystem/MainActivity.java
+++ SDKSystem/app/src/main/java/com/hitapps/sdksystem/MainActivity.java
@@ -1,18 +1,11 @@
 package com.hitapps.sdksystem;
 
-import android.*;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.hitapps.sdk.helper.SystemInfor;
 import com.hitapps.sdk.service.DuplicateShowService;
