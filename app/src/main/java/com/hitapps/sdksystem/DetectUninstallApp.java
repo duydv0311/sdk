25df82e10661e51a95ac75b7bd333e8a11f40b87
Duy <daovanduy0311@gmail.com>
2017-01-09 17:57:52 +0700

    zzzz

--- app/src/main/java/com/hitapps/sdksystem/DetectUninstallApp.java
+++ app/src/main/java/com/hitapps/sdksystem/DetectUninstallApp.java
@@ -1,19 +1,20 @@
 package com.hitapps.sdksystem;
 
 import android.app.Application;
+import android.os.Build;
 
-import com.coolerfall.watcher.Watcher;
+import cn.hiroz.uninstallfeedback.FeedbackUtils;
 
 /**
  * Created by Duy on 1/9/2017.
  */
 
 public class DetectUninstallApp extends Application {
-    private static final String URL = "http://www.baidu.com";
+
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Watcher.run(this, URL, true);
-    }
+        android.util.Log.e("DaemonThread", "Build Brand => " + Build.BRAND);
+        FeedbackUtils.openUrlWhenUninstall(this, "http://www.baidu.com");    }
 }
